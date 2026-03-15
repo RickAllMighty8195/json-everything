43825aaeb0a545462012b80e5519b4caad2d08c0
Greg Dennis <gregsdennis@yahoo.com>
2026-03-15 13:57:54 +1300

    handle sourcegen warnings

--- src/JsonSchema.Generation/SourceGeneration/JsonSchemaSourceGenerator.cs
+++ src/JsonSchema.Generation/SourceGeneration/JsonSchemaSourceGenerator.cs
@@ -7,6 +7,7 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
+#pragma warning disable RS1041
 
 namespace Json.Schema.Generation.SourceGeneration;
 
